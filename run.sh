99dbc77269a7012c9ce101a743062c44785df304
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2021-05-05 13:40:47 -0700

    Initial commit
    Created a basic script to establish SSH tunnel and run scrcpy

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# USAGE:
+# $0 ip port
+
+# Must have ADB 1.0.41 and scrcpy installed
+adb kill-server
+ssh root@$1 -L 5037:localhost:5037 -R 27183:localhost:27183 -p $2 &
+scrcpy
+
