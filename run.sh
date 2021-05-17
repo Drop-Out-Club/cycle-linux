3fcf8222c68b6cf47f1578c9df6b9bf874be3d99
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2021-05-17 12:34:34 -0700

    Removed almost all debug output

--- run.sh
+++ run.sh
@@ -21,31 +21,19 @@ SCREEN_Y=1080
 PUB_KEY=$(cat $3)
 BODY="{\"pub_key\": \"$PUB_KEY\", \"screen\":{\"res_x\": $SCREEN_X, \"res_y\": $SCREEN_Y}}"
 
-echo $PUB_KEY
-echo $BODY
-
-echo $REMOTE_IP
-
-echo -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d "$BODY"
-
-
 RESPONSE=$(curl $REMOTE_IP -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d "$BODY")
 
 #TODO: Verify director returned 200
 
 RESPONSE_BODY=$(echo $RESPONSE | jq .body | sed 's/\\//g')
 RESPONSE_BODY=$(echo $RESPONSE_BODY | sed 's/^.\(.*\).$/\1/')
-echo $RESPONSE_BODY
 
 CONTAINER_IP=$(echo $RESPONSE_BODY | jq .ip)
 CONTAINER_IP=$(echo $CONTAINER_IP | sed 's/^.\(.*\).$/\1/')
 CONTAINER_PORT=$(echo $RESPONSE_BODY | jq .port)
 
 
-echo $CONTAINER_IP:$CONTAINER_PORT
-
-echo DONE
-
+echo Connecting to: $CONTAINER_IP:$CONTAINER_PORT
 
 sleep 90
 
